202f8d658dfbf6582bde375057fe8925b689568e
Mbungai Michael bernard <97235186+MbungaiMichael@users.noreply.github.com>
2025-05-17 13:31:27 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-from pydantic import BaseModel
-from fastapi import FastAPI
-from app.model.source_code import *
+import pandas as pd
+import joblib
+import gradio as gr
 
 
 # Load the trained pipeline
 file_directory = r"C:\Users\UltraBook 3.1\PycharmProjects\Movie_rec2\cluster_pipeline_v1.0.0.pkl"
 pipeline = joblib.load(file_directory)
 
-app = FastAPI()
 
-
-# Input model for prediction
-class MovieData(BaseModel):
-    tag: str
-    genres: str
-    rating: float
-    mean: float
-    count: int
-
-
-@app.get("/")
-def home():
-    return {"Health Check": "OK", "model_version": version}
-
-
-@app.post("/predict_cluster")
-def predict_cluster(data: MovieData):
+def predict_cluster(tag, genres, rating, mean, count):
     # Convert input to DataFrame
-    input_df = pd.DataFrame([data.dict()])
+    input_data = [{"tag": tag, "genres": genres, "rating": rating, "mean": mean, "count": count}]
+    input_df = pd.DataFrame(input_data)
 
     # Transform and predict using the pipeline
     transformed = pipeline.transform(input_df)
@@ -37,3 +21,18 @@ def predict_cluster(data: MovieData):
         "cluster": int(cluster),
         "status": "success"
     }
+
+
+iface = gr.Interface(fn=predict_cluster,
+                     inputs=[
+                         gr.Textbox(label="Tag"),
+                         gr.Textbox(label="Genres"),
+                         gr.Slider(minimum=0, maximum=5, label="Rating"),
+                         gr.Slider(minimum=0, maximum=5, label="Mean"),
+                         gr.Slider(minimum=0, maximum=10000, label="Count")
+                     ],
+                     outputs='text',
+                     title="Movie Recommendation through Clustering"
+                     )
+
+iface.launch()
